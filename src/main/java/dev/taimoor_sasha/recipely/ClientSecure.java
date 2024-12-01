9fb84b158fc29db089151cc016c333d9f5e00452
Sasha11666 <basovasasa087@gmail.com>
2024-12-01 17:50:21 -0500

    added ClientSecure class

---
+++ src/main/java/dev/taimoor_sasha/recipely/ClientSecure.java
@@ -0,0 +1,13 @@
+package dev.taimoor_sasha.recipely;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientSecure {
+    private String id;
+    private String firstName;
+}
